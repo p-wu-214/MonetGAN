2ce4e729ce4f1eb9c4e900f3ca263a3aac7b425e
p-wu-214 <pattersonyou0214@gmail.com>
2020-12-31 21:26:41 -0800

    assertions pass on discriminator

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
+import tqdm
+
 from dataset.monet_dataset import MonetDataset
 
 from model.dcgan_generator_model import DCGanGeneratorModel
 
 from model.dcgan_discriminator_model import DCGanDiscriminatorModel
 
+from tensorflow.keras.utils import plot_model
+
+EPOCHS = 1
+
 IMAGE_WIDTH = 256
 IMAGE_HEIGHT = 256
-
+noise_dim = 100
 BATCH_SIZE = 8
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 directory = './data'
+
+# From logits = not softmaxed (not in range of 0-1) (not probabilities)
+CROSS_ENTROPY = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 
 def transform(example):
     feature_map = {
@@ -41,6 +50,19 @@ def display_image(num, monet_images, photo_images):
         plt.imshow(tf.cast(example_photo[i], tf.uint8))
     plt.show()
 
+
+def generator_loss_fn(disc_pred):
+    return CROSS_ENTROPY(tf.ones_like(disc_pred), disc_pred)
+
+def discriminator_loss_fn(generated, real):
+    generated_loss = CROSS_ENTROPY(tf.zeros_like(generated), generated)
+    real_loss = CROSS_ENTROPY(tf.ones(real), real)
+
+    return generated_loss + real_loss
+
+def __print_model(model, filename):
+    plot_model(model, to_file='{filename}.png'.format(filename=filename), show_shapes=True, show_layer_names=True)
+
 def main():
 
     monet_images_filenames = tf.io.gfile.glob(str(directory + '/monet_tfrec/*.tfrec'))
@@ -53,17 +75,36 @@ def main():
     photo = photo_ds.get()
 
     dcg = DCGanGeneratorModel()
-
     dcd = DCGanDiscriminatorModel()
 
     generator = dcg.make_generator_model()
-    noise = tf.random.normal([1, 100])
-    generated_example = generator(noise, training=False)
-
     discriminator = dcd.make_discriminator_model()
-    decision = discriminator(generated_example)
 
-    print(decision)
+    generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+    discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+
+    __print_model(generator, 'generator')
+    __print_model(discriminator, 'discriminator')
+
+    # for epoch in range(EPOCHS):
+    #     for mon, pho in tqdm(zip(monet, photo)):
+    #         with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+    #             generated_monet = generator(pho, training=True)
+    #
+    #             monet_pred = discriminator(mon, training=True)
+    #             generated_monet_pred = discriminator(generated_monet, training=True)
+    #
+    #             generator_loss = generator_loss_fn(generated_monet_pred)
+    #             discriminator_loss = discriminator_loss_fn(generated_monet_pred, monet_pred)
+    #         gradients_of_generator = gen_tape.gradient(generator_loss, generator.trainable_variables)
+    #         gradients_of_discriminator = disc_tape.gradient(discriminator_loss, discriminator.trainable_variables)
+    #
+    #         generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+    #         discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+
+
+
+
 
 
 
--- model/dcgan_generator_model.py
+++ model/dcgan_generator_model.py
@@ -6,35 +6,43 @@ class DCGanGeneratorModel:
 
     def make_generator_model(self):
         model = tf.keras.Sequential()
-        model.add(layers.Dense(units=16*16*256, use_bias=False, input_shape=(100,)))
+
+        model.add(layers.Conv2DTranspose(3, kernel_size=3, strides=1, padding='same',
+                                         use_bias=False, input_shape=(256, 256, 3)))
+        assert model.output_shape == (None, 256, 256, 3)
         model.add(layers.BatchNormalization())
         model.add(layers.LeakyReLU())
 
-        model.add(layers.Reshape((16, 16, 256)))
-        assert model.output_shape == (None, 16, 16, 256)
-
-        model.add(layers.Conv2DTranspose(128, kernel_size=(5, 5), strides=(1, 1), padding='same', use_bias=False))
-        assert model.output_shape == (None, 16, 16, 128)
+        model.add(layers.Conv2D(18, kernel_size=4, strides=2, padding='same',
+                                         use_bias=False))
+        print(model.output_shape)
+        assert model.output_shape == (None, 128, 128, 18)
         model.add(layers.BatchNormalization())
         model.add(layers.LeakyReLU())
 
-        model.add(layers.Conv2DTranspose(64, kernel_size=(5, 5), strides=(2, 2), padding='same', use_bias=False))
-        assert model.output_shape == (None, 32, 32, 64)
+        model.add(layers.Conv2D(36, kernel_size=4, strides=2, padding='same',
+                                         use_bias=False))
+        assert model.output_shape == (None, 64, 64, 36)
         model.add(layers.BatchNormalization())
         model.add(layers.LeakyReLU())
 
-        model.add(layers.Conv2DTranspose(32, kernel_size=(5, 5), strides=(2, 2), padding='same', use_bias=False))
-        assert model.output_shape == (None, 64, 64, 32)
+        model.add(layers.Conv2D(72, kernel_size=4, strides=2, padding='same', use_bias=False))
+        assert model.output_shape == (None, 32, 32, 72)
         model.add(layers.BatchNormalization())
         model.add(layers.LeakyReLU())
 
-        model.add(layers.Conv2DTranspose(16, kernel_size=(5, 5), strides=(2, 2), padding='same', use_bias=False))
-        assert model.output_shape == (None, 128, 128, 16)
+        model.add(layers.Conv2DTranspose(36, kernel_size=4, strides=2, padding='same', use_bias=False))
+        assert model.output_shape == (None, 64, 64, 36)
         model.add(layers.BatchNormalization())
         model.add(layers.LeakyReLU())
 
-        model.add(layers.Conv2DTranspose(1, kernel_size=(5, 5), strides=(2, 2), padding='same', use_bias=False))
-        assert model.output_shape == (None, 256, 256, 1)
+        model.add(layers.Conv2DTranspose(18, kernel_size=4, strides=2, padding='same', use_bias=False))
+        assert model.output_shape == (None, 128, 128, 18)
+        model.add(layers.BatchNormalization())
+        model.add(layers.LeakyReLU())
+
+        model.add(layers.Conv2DTranspose(3, kernel_size=4, strides=2, padding='same', use_bias=False))
+        assert model.output_shape == (None, 256, 256, 3)
         model.add(layers.BatchNormalization())
         model.add(layers.LeakyReLU())
 
